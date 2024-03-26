27980d79a7ecb1d5ce21df3735a678a87a73267b
Dmitrii Timofeev <28dngn@gmail.ru>
2024-03-27 00:08:04 +0300

    3 and 4 fixed

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,14 +1,7 @@
 function ucFirst(str) {
-  if (typeof str) {
-    str = str.toLowerCase();
-  } else {
-    return null;
-  }
-  
-  if (typeof str === 'string') {
-    str = str.trim();
-    str = str.slice(0, 1).toUpperCase() + str.slice(1);
+  if (!str) {
+    return '';
   }
 
-  return str;
+  return str.slice(0, 1).toUpperCase() + str.slice(1);
 }
