d4a3425cfa4394cf9a8750c0a43c2501a810fd09
Dmitrii Timofeev <28dngn@gmail.ru>
2024-03-26 16:09:56 +0300

    1-5 tasks

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,14 @@
 function ucFirst(str) {
-  // ваш код...
+  if (typeof str) {
+    str = str.toLowerCase();
+  } else {
+    return null;
+  }
+  
+  if (typeof str === 'string') {
+    str = str.trim();
+    str = str.slice(0, 1).toUpperCase() + str.slice(1);
+  }
+
+  return str;
 }
