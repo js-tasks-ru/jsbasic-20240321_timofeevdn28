8d192df16c257ef31c8a345acfbd22b62237a331
Dmitrii Timofeev <28dngn@gmail.ru>
2024-04-06 00:58:35 +0300

    added 1-3 tasks module 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ulElement = document.createElement('ul');
+
+  for (let frend of friends) {
+    let liElement = `<li>${frend.firstName} ${frend.lastName}</li>`;
+    ulElement.insertAdjacentHTML('beforeend', liElement);
+  }
+
+  return ulElement;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,48 @@
+function checkAge(cell) {
+  if (cell.textContent < 18) {
+    cell.style.textDecoration = 'line-through';
+  }
+}
+
+function checkGender(cell) {
+  switch (cell.textContent) {
+  case 'm':
+    cell.classList.add('male');
+    return;
+  case 'f':
+    cell.classList.add('female');
+    return;
+  }
+}
+
+function checkStatus(cell) {
+  if (cell.dataset.available === 'true') {
+    cell.classList.add('available');
+  } else {
+    cell.classList.add('unavailable');
+  }
+}
+
+
 function highlight(table) {
-  // ваш код...
+  let CellNumberAge = 1;
+  let CellNumberGender = 2;
+  let CellNumberStatus = 3;
+
+  for (let i = 0; i < table.tBodies[0].rows.length; i++) {
+    let row = table.tBodies[0].rows[i];
+
+    let cellAge = row.cells[CellNumberAge];
+    checkAge(cellAge);
+
+    let cellGender = row.cells[CellNumberGender];
+    checkGender(cellGender);
+
+    let cellStatus = row.cells[CellNumberStatus];
+    if (cellStatus.hasAttribute('data-available')) {
+      checkStatus(cellStatus);
+    } else {
+      cellStatus.hidden = 'true';
+    }
+  }
 }
