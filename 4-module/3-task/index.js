80d0cf80286805cbc81d330254a38f9d43a28c47
Dmitrii Timofeev <28dngn@gmail.ru>
2024-04-06 13:37:04 +0300

    task 3 module 4 fixed

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,48 +1,30 @@
-function checkAge(cell) {
-  if (cell.textContent < 18) {
-    cell.style.textDecoration = 'line-through';
-  }
-}
-
-function checkGender(cell) {
-  switch (cell.textContent) {
-  case 'm':
-    cell.classList.add('male');
-    return;
-  case 'f':
-    cell.classList.add('female');
-    return;
-  }
-}
-
-function checkStatus(cell) {
-  if (cell.dataset.available === 'true') {
-    cell.classList.add('available');
-  } else {
-    cell.classList.add('unavailable');
-  }
-}
-
-
 function highlight(table) {
-  let CellNumberAge = 1;
-  let CellNumberGender = 2;
-  let CellNumberStatus = 3;
+  let CellAge = 1;
+  let CellGender = 2;
+  let CellStatus = 3;
 
   for (let i = 0; i < table.tBodies[0].rows.length; i++) {
     let row = table.tBodies[0].rows[i];
 
-    let cellAge = row.cells[CellNumberAge];
-    checkAge(cellAge);
+    if (row.cells[CellAge].textContent < 18) {
+      row.style.textDecoration = 'line-through';
+    }
 
-    let cellGender = row.cells[CellNumberGender];
-    checkGender(cellGender);
+    if (row.cells[CellGender].textContent === 'm') {
+      row.classList.add('male');
+    }
+    if (row.cells[CellGender].textContent === 'f') {
+      row.classList.add('female');
+    }
 
-    let cellStatus = row.cells[CellNumberStatus];
-    if (cellStatus.hasAttribute('data-available')) {
-      checkStatus(cellStatus);
+    if (row.cells[CellStatus].hasAttribute('data-available')) {
+      if (row.cells[CellStatus].dataset.available === 'true') {
+        row.classList.add('available');
+      } else {
+        row.classList.add('unavailable');
+      }
     } else {
-      cellStatus.hidden = 'true';
+      row.hidden = 'true';
     }
   }
 }
