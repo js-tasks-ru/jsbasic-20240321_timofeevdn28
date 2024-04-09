53d301dea56959d6c86725195af69a785da65f4b
Dmitrii Timofeev <28dngn@gmail.ru>
2024-04-09 16:33:48 +0300

    added tasks 1-3 module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+
+  btn.addEventListener('click', () => {
+    text.hidden = !text.hidden;
+  });
 }
